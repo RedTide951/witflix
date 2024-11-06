be5b57f3ee97a613c03ad91d5ffd5b4eaa3db645
RedTide951 <oguzhanraifbutuner@gmail.com>
2024-11-06 09:08:25 +0300

    js basic

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+  const menu = document.getElementById("hamburger-menu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function toggleSearch() {
+  const searchBar = document.querySelector(".search-bar");
+  if (searchBar.style.width === "150px") {
+    searchBar.style.width = "0";
+    searchBar.style.opacity = "0";
+  } else {
+    searchBar.style.width = "150px";
+    searchBar.style.opacity = "1";
+  }
+}
